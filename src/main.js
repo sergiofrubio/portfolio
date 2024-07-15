f77ec237ff8b2c8c57d44c473de2ae4b051d3272
sergiofrubio <sergiofrubio@gmail.com>
2024-07-15 09:44:47 +0200

    import bootstrap.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '@/assets/scss/styles.scss'
+import 'bootstrap/dist/js/bootstrap.bundle'
 
 const app = createApp(App)
 
