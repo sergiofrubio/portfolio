1124f21612f7b1d773374e35cfced9d9d8b98d9b
sergiofrubio <sergiofrubio@gmail.com>
2024-08-20 18:47:02 +0200

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sergiofrubio-portfolio/",
+  base: "/portfolio/",
   plugins: [
     vue(),
   ],
